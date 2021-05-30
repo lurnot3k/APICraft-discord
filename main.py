0dcf8ae443ef254aa0870eaa0ff76e5f337979d1
Lurnot3k <dev@lurnot3k.fr>
2021-05-30 07:56:18 +0200

    Améliorations générales

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def on_message(message):
 				await message.channel.send(embed = embed)
 
 		if message.content.startswith('-help'):
-			await message.channel.send("**Les commandes du bot sont :**\n\n`-profil [pseudo]` pour afficher le profil Minecraft d'un joueur.\n`-hypixel` pour afficher les infos sur le serveur hypixel.\n`-paladium` pour afficher les infos sur le serveur Paladium.")
+			await message.channel.send("**Les commandes du bot sont :**\n\n`-profil [pseudo]` pour afficher le profil Minecraft d'un joueur.\n`-hypixel` pour afficher les infos sur le serveur hypixel.\n`-paladium` pour afficher les infos sur le serveur Paladium.\n`-brickmc` affiche les infos sur le serveur BrickMC réalisé par iMot3k.")
 
 		if message.content.startswith("-hypixel"):
 			hypixel_online = requests.get("https://minecraft-api.com/api/ping/status/mc.hypixel.net/25565")
@@ -61,11 +61,11 @@ async def on_message(message):
 			page3 = soup4.find("body")
 			
 			if page.string == "En ligne":
-				status =f"`{page.string}` :green_circle:"
+				status ="`En Ligne` :green_circle:"
 				embed = discord.Embed(title="", description="", color=0x1ac447)
 
 			else:
-				status =f"`{page.string}` :red_circle:"
+				status ="`Hors Ligne` :red_circle:"
 				embed = discord.Embed(title="", description="", color=0xfc033d)
 
 			if not page2:
@@ -75,7 +75,7 @@ async def on_message(message):
 				onlineplayers = f"{page2.string}"
 			
 			if not page3:
-    				serverver = "Serveur hors-ligne"
+    				serverver = "Serveur hors ligne"
 			
 			else:
 				serverver = f"{page3.string}"[12:]
@@ -88,26 +88,26 @@ async def on_message(message):
 			await message.channel.send(embed = embed)
 
 		if message.content.startswith("-paladium"):
-			hypixel_online = requests.get("https://eu.mc-api.net/v3/server/status-http/proxy.paladium-pvp.fr")
-			content = hypixel_online.content
+			paladium_online = requests.get("https://api.serveurs-minecraft.com/api.php?Etat_Query&ip=proxy.paladium-pvp.fr&port=25565")
+			content = paladium_online.content
 			soup2 = BeautifulSoup(content, features="lxml")
 			page = soup2.find("body")
-			hypixel_players = requests.get("https://api.serveurs-minecraft.com/api.php?Joueurs_En_Ligne_Ping&ip=proxy.paladium-pvp.fr&port=25565")
-			content2 = hypixel_players.content
+			paladium_players = requests.get("https://api.serveurs-minecraft.com/api.php?Joueurs_En_Ligne_Ping&ip=proxy.paladium-pvp.fr&port=25565")
+			content2 = paladium_players.content
 			soup3 = BeautifulSoup(content2, features="lxml")
 			page2 = soup3.find("body")
-			hypixel_ver = requests.get("http://api.serveurs-minecraft.com/api.php?Version_Ping&ip=proxy.paladium-pvp.fr&port=25565")
-			content3 = hypixel_ver.content
+			paladium_ver = requests.get("http://api.serveurs-minecraft.com/api.php?Version_Ping&ip=proxy.paladium-pvp.fr&port=25565")
+			content3 = paladium_ver.content
 			soup4 = BeautifulSoup(content3, features="lxml")
 			page3 = soup4.find("body")
 			
-			if page.string.startswith("true"):
-				status = "`En ligne` :green_circle:"
-				embed = discord.Embed(title="", description="", color=0x1ac447)
+			if not page:
+				status ="`Hors Ligne` :red_circle:"
+				embed = discord.Embed(title="", description="", color=0xfc033d)
 
 			else:
-				status = "`Hors Ligne` :red_circle:"
-				embed = discord.Embed(title="", description="", color=0xfc033d)
+				status ="`En Ligne` :green_circle:"
+				embed = discord.Embed(title="", description="", color=0x1ac447)
 			
 			if not page2:
     				onlineplayers = "0"
@@ -116,7 +116,7 @@ async def on_message(message):
 				onlineplayers = f"{page2.string}"
 			
 			if not page3:
-    				serverver = "Serveur hors-ligne"
+    				serverver = "Serveur hors ligne"
 			
 			else:
 				serverver = f"{page3.string}"
@@ -128,7 +128,7 @@ async def on_message(message):
 			embed.add_field(name ="Nombre de joueurs", value =f"`{onlineplayers}`", inline =False)
 			embed.add_field(name ="Version du serveur", value =f"`{serverver}`" )
 			await message.channel.send(embed = embed)
-			
+
 		if message.content.startswith("-brickmc"):
 			brickmc_online = requests.get("https://eu.mc-api.net/v3/server/status-http/mc.imot3k.fr")
 			content = brickmc_online.content
@@ -139,13 +139,13 @@ async def on_message(message):
 			soup3 = BeautifulSoup(content2, features="lxml")
 			page2 = soup3.find("body")
 			
-			if page.string.startswith("true"):
-				status = "`En ligne` :green_circle:"
-				embed = discord.Embed(title="", description="", color=0x1ac447)
+			if not page:
+				status =f"`Hors Ligne` :red_circle:"
+				embed = discord.Embed(title="", description="", color=0xfc033d)
 
 			else:
-				status = "`Hors Ligne` :red_circle:"
-				embed = discord.Embed(title="", description="", color=0xfc033d)
+				status ="`En Ligne` :green_circle:"
+				embed = discord.Embed(title="", description="", color=0x1ac447)
 			
 			if not page2:
     				onlineplayers = "0"
@@ -155,7 +155,7 @@ async def on_message(message):
 				
 
 			if not page2:
-					serverver = "Serveur hors-ligne"
+					serverver = "Serveur hors ligne"
 			
 			else:
 				serverver = "1.16.1"
@@ -165,7 +165,8 @@ async def on_message(message):
 			embed.add_field(name ="Statut", value =f"{status}", inline =False)
 			embed.add_field(name ="Nombre de joueurs", value =f"`{onlineplayers}`", inline =False)
 			embed.add_field(name ="Version du serveur", value =f"`{serverver}`" )
-			await message.channel.send(embed = embed)		
+			await message.channel.send(embed = embed)	
+
 
 
 client.run('TOKEN')
